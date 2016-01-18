92ea7823d3fb39970b19eea202dc234b7e8fc5e6
Mariyan Borisov <marian.borisov@gmail.com>
2016-01-18 22:03:13 +0200

    Switch from tabs to spaces

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,7 +1,7 @@
 EXCHANGE_RATES = {usd: 1.7408, eur: 1.9557, gbp: 2.6415}
 
 def convert_to_bgn(price, currency)
-	(price * EXCHANGE_RATES.fetch(currency, 1)).round(2)
+  (price * EXCHANGE_RATES.fetch(currency, 1)).round(2)
 end
 
 def compare_prices(first_price, first_currency, second_price, second_currency)
